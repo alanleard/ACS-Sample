fa1b244a013ab74802a93e37b6680587b9fb6f45
Alan Leard <aleard@appcelerator.com>
2012-05-01 01:36:06 -0700

    Removed data manipulation from listView

--- Resources/ui/detail/listView.js
+++ Resources/ui/detail/listView.js
@@ -1,37 +1,47 @@
-exports.list = function(model){
-	var model = require('models/'+model);
-	
-	var animate = require('modules/animation');
-	
-	var table = Ti.UI.createTableView({
-        backgroundColor: '#000',
+var style = require('ui/common/style');
+
+var animate = require('modules/animation');
+
+exports.rows = function(e){
+	var data = [];
+	for (var i = 0, l = e.length; i < l; i++) {
+		data.push(
+			Ti.UI.createTableViewRow({
+				backgroundColor:style.color2,
+		        color:style.color3,
+		        title:e[i].title,
+		        id:e[i].id || null
+   		 	})
+   		 	
+   		);
+
+  	 }
+  	 return data;
+    
+}
+
+exports.list = function(data, view){
+	//var model = require('models/'+model);
+    
+    var table = Ti.UI.createTableView({
+        backgroundColor: style.color2,
         top: 0, bottom: 0,
-        data: [
-            { title: 'Loading, please wait...' }
-        ]
+        data:data
     });
-	
-	model.query(function(e){
-		var data = [];
-		for (var i = 0, l = e.length; i < l; i++) {
-                        data.push(Ti.UI.createTableViewRow({
-                            title: e[i].title,
-                            id: e[i].id,
-                            backgroundColor:'#000',
-                            color:'#fff'
-                        }));
-                 
-                    }
-       table.setData(data);
-	});
+    
+	// model.query(function(e){
+// 		
+       // table.setData(rows(e));
+	// });
 
 	return table;
 }
  
-exports.refresh = function(model, callback){
- 	var model = require('models/'+model);
- 	
- 	model.query(function(e){
- 		callback(e)
- 	});
-}
+// exports.refresh = function(model, callback){
+ 	// var model = require('models/'+model);
+//  	
+ 	// model.query(function(e){
+//  		
+ 		// callback(e);
+ 	// });
+// }
