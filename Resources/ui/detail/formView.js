2c5f831ea2c4a1b645b39197043a4090ea2ebc46
Alan Leard <aleard@appcelerator.com>
2012-05-01 01:36:38 -0700

    Modified pickers for duration and Android

--- Resources/ui/detail/formView.js
+++ Resources/ui/detail/formView.js
@@ -9,7 +9,7 @@ exports.create = function(model, callback){
 	
 	var bgView = Ti.UI.createView({backgroundColor:'black', opacity:0.8});
 	
-	var formView = Ti.UI.createView({top:20, bottom: 20, left:10, right:10});
+	var formView = Ti.UI.createView({top:20, bottom: 20, left:0, right:0});
 	
 	var popView = Ti.UI.createView({top:20, bottom:20, left:10, right:10, borderRadius:20, visible:false});
 
@@ -51,7 +51,7 @@ exports.create = function(model, callback){
     var pop = function(model){
     	var text = Ti.UI.createTextField({
 				hintText: data[i].hint,
-		        top: 10 , left: 10 , right: 10 ,
+		        top: 10 , left: 20 , right: 20 ,
 		        height: 40 ,
 		        borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
 		        required:data[i].required
@@ -60,6 +60,7 @@ exports.create = function(model, callback){
 			content.add(text);
 			var field = fields.length;
 			text.addEventListener('focus', function(){
+				text.blur();
 				var list = listView.list(model);
 				popView.add(list);
 				popView.show();
@@ -77,7 +78,7 @@ exports.create = function(model, callback){
 	    if(data[i].type=='text'){
 		    var text = Ti.UI.createTextField({
 				hintText: data[i].hint,
-		        top: 10 , left: 10 , right: 10 ,
+		        top: 10 , left: 20 , right: 20 ,
 		        height: 40 ,
 		        borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
 		        required:data[i].required
@@ -85,29 +86,62 @@ exports.create = function(model, callback){
 			fields.push(text);
 			content.add(text);
 		} else if (data[i].type == 'picker_date'){
-			var picker_date = Ti.UI.createPicker({
-			  type:Ti.UI.PICKER_TYPE_DATE_AND_TIME,
-			  minDate:new Date(),
-			  minuteInterval:15
-			});
-			fields.push(picker_date);
-			content.add(picker_date);
+			if(Ti.Platform.osname != 'android'){
+				var picker_date = Ti.UI.createPicker({
+				  type:Ti.UI.PICKER_TYPE_DATE,
+				  minDate:new Date(),
+				  minuteInterval:15,
+				  value:new Date(),
+				});
+				if(Ti.Platform.osname != 'android'){
+					picker_date.setType(Ti.UI.PICKER_TYPE_DATE_AND_TIME);
+				}
+				
+				picker_date.addEventListener('change', function(){
+					picker_time.setMinDate(picker_date.value);
+					picker_time.setValue(picker_date.value);
+				});
+				fields.push(picker_date);
+				content.add(picker_date);
+			}
 		} else if (data[i].type == 'picker_time'){
-			var picker_time = Ti.UI.createPicker({
-			  type:Ti.UI.PICKER_TYPE_TIME,
-			  minuteInterval:15
-			});
-			fields.push(picker_time);
-			content.add(picker_time);
+			//if(Ti.Platform.osname == 'android'){
+				var picker_time = Ti.UI.createPicker({
+				  type:Ti.UI.PICKER_TYPE_TIME,
+				  minuteInterval:15,
+				  minDate:new Date(),
+				  value:new Date(),
+				});
+				if(Ti.Platform.osname != 'android'){
+					picker_time.setType(Ti.UI.PICKER_TYPE_DATE_AND_TIME);
+				}
+				picker_time.addEventListener('change', function(){});
+				fields.push(picker_time);
+				content.add(picker_time);
+				
+				
+			//}
 		} else if (data[i].type == 'artists'){
 			 pop('artists');
 			
 		} else if (data[i].type == 'venues'){
 			
 			pop('venues');
+		} else if(data[i].type == 'password'){
+			var text = Ti.UI.createTextField({
+				hintText: data[i].hint,
+		        top: 10 , left: 20 , right: 20 ,
+		        height: 40 ,
+		        borderStyle: Ti.UI.INPUT_BORDERSTYLE_ROUNDED,
+		        required:data[i].required,
+		        passwordMask:true
+			});
+			fields.push(text);
+			content.add(text);
 		}
 		
 	}
+	
 	var button = Ti.UI.createButton({
         title: 'Create',
         top: 10 , left: 10 , right: 10 , bottom: 10 ,
